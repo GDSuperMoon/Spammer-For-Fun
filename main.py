c99bf337628e099316f4e42246e1b6defd6c8972
[GD]SuperMoon Super Presents <zenja.kovolevskis@yandex.ru>
2020-07-14 15:10:59 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import os, time
+print("Hello and welcome to spammer!")
+time.sleep(2)
+command = "clear"
+os.system(command)
+spam = input("Input any word or number to spam: ")
+
+while True:
+  print(spam)
